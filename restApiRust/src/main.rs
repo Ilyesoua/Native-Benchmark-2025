5b2b0fccbad2eb059471c5b93b7b9b571d6e1117
Ilyes OUANEZAR <ilyes.ouanezar@proximus.lu>
2025-05-06 11:07:33 +0200

    [dev] new features to get the final measurements

--- restApiRust/src/main.rs
+++ restApiRust/src/main.rs
@@ -1,9 +1,11 @@
 use actix_web::{App, HttpServer, web};
 use dotenv::dotenv;
 use diesel::r2d2::{self, ConnectionManager};
-use crate::service::MovieService;
 use diesel::PgConnection;
 use std::sync::Arc;
+use std::time::Instant;
+
+use crate::service::MovieService;
 
 mod model;
 mod repository;
@@ -13,18 +15,18 @@ mod schema;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let startup_start = Instant::now(); // ⏱️ début chrono
+
     dotenv().ok();
     let movie_service = Arc::new(MovieService::new());
 
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
     let manager = ConnectionManager::<PgConnection>::new(database_url);
-
     let pool = r2d2::Pool::builder()
         .build(manager)
         .expect("Failed to create pool.");
 
-    HttpServer::new(move || {
+    let server = HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
             .app_data(web::Data::from(movie_service.clone()))
@@ -35,7 +37,10 @@ async fn main() -> std::io::Result<()> {
             .service(controllers::movies::get_movie_by_id)
             .service(controllers::movies::random_movie)
     })
-    .bind(("0.0.0.0", 8080))?
-    .run()
-    .await
+    .bind(("0.0.0.0", 8080))?;
+
+    let startup_duration = startup_start.elapsed();
+    println!("🚀 Server startup completed in {:.2?}", startup_duration);
+
+    server.run().await
 }
